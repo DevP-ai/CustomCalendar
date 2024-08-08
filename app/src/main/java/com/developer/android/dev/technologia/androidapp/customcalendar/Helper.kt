1adaa8501de396039423aed41861d477c31494b6
Devajit <patar1447@gmail.com>
2024-08-09 01:12:08 +0530

    A helper function created for check current

--- app/src/main/java/com/developer/android/dev/technologia/androidapp/customcalendar/Helper.kt
+++ app/src/main/java/com/developer/android/dev/technologia/androidapp/customcalendar/Helper.kt
@@ -1,6 +1,8 @@
 package com.developer.android.dev.technologia.androidapp.customcalendar
 
 import android.content.Context
+import org.joda.time.DateTime
+import java.util.Date
 
 fun getMonth(month:Int,context:Context):String?{
     when(month){
@@ -18,4 +20,24 @@ fun getMonth(month:Int,context:Context):String?{
         12->return context.resources.getString(R.string.dec)
     }
     return  null
+}
+
+fun createDate(day:Int,monthOfYear:Int,year:Int):DateTime?{
+    return  DateTime()
+        .withYear(year)
+        .withMonthOfYear(monthOfYear)
+        .withDayOfMonth(day)
+        .withTime(0,0,0,0)
+}
+
+fun isItToday(date:DateTime):Int{
+    val dtToCompare = date.withTime(0,0,0,0)
+    val dtToday = DateTime(Date()).withTime(0,0,0,0)
+
+    if(dtToCompare.isAfter(dtToday)){
+        return  0
+    }else if(dtToCompare.isEqual(dtToday)){
+        return 1
+    }
+    return 2
 }
